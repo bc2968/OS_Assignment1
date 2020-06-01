14245eff522e3eb95eb8ea72440c3310a65a0d4f
B chen <bc2968@.nyu.edu>
2020-06-01 13:30:19 -1000

    adding helloworld.c

---
+++ hello.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+   // printf() displays the string inside quotation
+   //chaning file for github test
+   //changing file while other people are also changin file. out of sync
+   printf("Hello World");
+   //making a local changes
+   return 0;
+}
