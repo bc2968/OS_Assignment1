32013fca9c21661f2b0e71c980abecf034a03171
B chen <bc2968@.nyu.edu>
2020-06-01 14:02:00 -1000

    changes

--- hello.c
+++ hello.c
@@ -4,7 +4,9 @@ int main() {
    //chaning file for github test
    //changing file while other people are also changin file. out of sync
    printf("Hello World");
+   printf("adding to local branch");
    //making a local changes
+   printf("adding to local branch again");
    return 0;
    //adding some change to local branch
 }
